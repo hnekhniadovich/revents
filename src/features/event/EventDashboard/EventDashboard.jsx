c2ac89175336ae622cfe6c47d6b9808894eeca01
hnekhniadovich <nekhanya@gmail.com>
2019-04-06 18:59:41 -0400

    handle model state

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -54,18 +54,36 @@ const eventsDashboard = [
     }
 ]
 
-
 class EventDashboard extends Component {
+    state = {
+        events: eventsDashboard,
+        isOpen: false
+    }
+    
+
+    handleFormOpen = () => {
+        this.setState({
+            isOpen: true
+        })
+    }
+
+    handleCancel = () => {
+        this.setState({
+            isOpen: false
+        })
+    }
+
     render() {
         return (
             <div>
                 <Grid>
                     <Grid.Column width={10}>
-                        <EventList events={eventsDashboard}/>
+                        <EventList events={this.state.events}/>
                     </Grid.Column>
                     <Grid.Column width={6}>
-                        <Button positive content="Create Event" />
-                        <EventForm />
+                        <Button onClick={this.handleFormOpen} positive content="Create Event" />
+                        {this.state.isOpen && 
+                        <EventForm handleCancel={this.handleCancel}/>}
                     </Grid.Column>
                 </Grid>
             </div>
