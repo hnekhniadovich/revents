d0a253db6c6c3729df9c53f83eb281cc34e06fb5
hnekhniadovich <nekhanya@gmail.com>
2019-04-10 13:44:44 -0400

    redux initital setup

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -9,6 +9,7 @@ import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedP
 import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
 import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
 import HomePage from '../../features/home/HomePage';
+import TestComponent from '../../features/testarea/TestComponent';
 
 
 class App extends Component {
@@ -27,6 +28,7 @@ class App extends Component {
               <Container className="main">
                 <Switch>
                   <Route path='/events' component={EventDashboard} />
+                  <Route path='/test' component={TestComponent} />
                   <Route path='/events/:id' component={EventDetailedPage} />
                   <Route path='/people' component={PeopleDashboard} />
                   <Route path='/profile/:id' component={UserDetailedPage} />
